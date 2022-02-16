047c7803da2a47b0677ddb5d90ca2bcb13578557
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-16 11:56:10 -0700

    Add classname to reservation card div

--- src/Reservations.js
+++ src/Reservations.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Reservations = ({name, date, time, number}) => {
   return (
-    <div>
+    <div className='reservation-card'>
     <h2>{name}</h2>
     <h4>{date}</h4>
     <h4>{time}</h4>
