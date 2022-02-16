34b1d48604e6a7728891f05111401dfed86f0871
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-16 09:45:42 -0700

    Create component for reservation card

---
+++ src/Reservations.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Reservations = ({name, date, time, number}) => {
+  return (
+    <div>
+    <h2>{name}</h2>
+    <h4>{date}</h4>
+    <h4>{time}</h4>
+    <h4>{number}</h4>
+    </div>
+  )
+}
+
+
+export default Reservations;
